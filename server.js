c9d66930d4fb25549a4653926600251489a6e538
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-05-06 17:21:11 +0530

    admin pannel

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 
 //app config
@@ -18,6 +19,7 @@ app.use(cors())
 
 
 //api endpoint
+app.use('/api/admin',adminRouter)
 
 
 app.get('/',(req,res)=>{
