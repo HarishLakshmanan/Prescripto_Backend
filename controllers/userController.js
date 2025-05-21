c954566251e53c5d8bb506b6b0928282e0b18b57
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-05-21 19:54:09 +0530

    add usercontroller and userRoutes

---
+++ controllers/userController.js
@@ -0,0 +1,82 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import userModel from '../model/userModel.js'
+import jwt from 'jsonwebtoken'
+
+
+//API TO REGISTER USER
+
+const registerUser = async (req, res) => {
+    try {
+
+        const { name, email, password } = req.body
+
+        if (!name || !password || !email) {
+            return res.json({ success: false, message: "Missing Details" })
+        }
+
+
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Enter a Valid Email" })
+        }
+
+
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Enter a Valid Password" })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hasedPassword = await bcrypt.hash(password, salt)
+
+        const userData = {
+            name, email, password: hasedPassword
+        }
+
+        const newUser = new userModel(userData)
+        const user = await newUser.save()
+
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+
+        res.json({ success: true, token })
+
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//API FOR USER LOGIN
+
+const loginUser = async (req, res) => {
+    try {
+
+
+        const { email, password } = req.body
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.json({ success: false, message: "User does not exist" })
+
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if(isMatch){
+            const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }else{
+            res.json({success:false,message:'Invalid credentials'})
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+
+    }
+}
+
+
+export { registerUser ,loginUser}
